823f24341ad368ad64e266f82586e871460de7d0
Pedro Mion <pedromionbraga@gmail.com>
2023-08-28 20:14:11 -0300

    Allowing the coins to be eaten

--- snakeGame.py
+++ snakeGame.py
@@ -19,9 +19,12 @@ class Snake(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load("Cabeca.png")
+        self.bodyImage = pygame.image.load("Corpo.png")
         self.rect = self.image.get_rect()
         self.rect.center = (250,250)
         self.direction = [5, 0]
+        self.bodyParts = []
+        self.positions = []
     
     def update(self):
         pressed_keys = pygame.key.get_pressed()
@@ -36,16 +39,33 @@ class Snake(pygame.sprite.Sprite):
             self.direction = [5, 0]
     
     def move(self):
+        self.positions.insert(0, self.rect.center)
+        if(len(self.positions) > len(self.bodyParts) + 1):
+            self.positions.pop()
+
         self.rect.move_ip(self.direction[0], self.direction[1])
+        for i in range(len(self.bodyParts)):
+            self.bodyParts[i].center = self.positions[i]
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
+        for bodyPart in self.bodyParts:
+            surface.blit(self.bodyImage, bodyPart)
     
     def isGameRunning(self):
         if self.rect.left <= 0 or self.rect.right >= SCREEN_WIDTH or self.rect.top <= 0 or self.rect.bottom >= SCREEN_HEIGHT:
             return False
         return True
 
+    def checkCoin(self, Coin):
+        collide = self.rect.colliderect(Coin.rect)
+        if collide:
+            newPart = pygame.image.load("Corpo.png")
+            newPartRect = newPart.get_rect()
+            newPartRect.center = self.positions[-1]
+
+            self.bodyParts.append(newPartRect)
+            Coin.newPosition()
 class Coin(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -58,6 +78,9 @@ class Coin(pygame.sprite.Sprite):
     
     def draw(self, surface):
         surface.blit(self.image, self.rect)
+    
+    def newPosition(self):
+        self.rect.center = (random.randint(10, SCREEN_WIDTH-10), random.randint(10, SCREEN_HEIGHT-10))
 
 Player1 = Snake()
 Coin1 = Coin()
@@ -71,6 +94,7 @@ while playerAlive: #principal loop
         pygame.display.update()
     Player1.update()
     Player1.move()
+    Player1.checkCoin(Coin1)
     
     DISPLAYSURF.fill((0,0,0))
 
